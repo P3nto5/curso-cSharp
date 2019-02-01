9767aca8d6b5ebd00997b63f0bd5eb0e7fac83b3
samuel <sam226gold@gmail.com>
2019-02-01 17:15:40 -0400

    Programa de banco feito

--- c# poo/Construtores_Banco/Program.cs
+++ c# poo/Construtores_Banco/Program.cs
@@ -12,56 +12,78 @@ namespace Construtores_Banco
         static void Main(string[] args)
         {
             Global conta;
-            Double quantia;
+          
 
             Console.WriteLine("Entre com o numero da conta");
             int numero = int.Parse(Console.ReadLine());
+            //Entrada do numero da conta
 
-            Console.WriteLine("Entre com o titular da conta");
+            Console.WriteLine("Entre com o nome titular da conta");
             string nome = Console.ReadLine();
+            //Entrada com o nome do titular da conta
 
             Console.WriteLine("Deseja depositar");
             string Opçao = Console.ReadLine();
-
-            if(Opçao == "Sim" || Opçao =="sim")
+            //Entrada de verificaçao se deseja depositar
+            if(Opçao == "sim" || Opçao =="Sim")
             {
                 Console.WriteLine("Digite o deposito inicial");
                 Double deposito = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                //leitura do deposito
                 conta = new Global(numero, nome, deposito);
-
-                Console.WriteLine("voce deseja depositar novamente ?");
-                
-                string Opçao_New = Console.ReadLine();
-                if (Opçao_New == "Sim" || Opçao_New != "nao")
+                //instanciando o objeto e passando por parametro os atributos 
+                Console.WriteLine("Dados atualizados: \n" + conta);
+                //printando na tela os dados atualizados
+                Console.WriteLine("voce deseja depositar novamente ?");                
+                string Opçao_Novo_Deposito = Console.ReadLine();
+                //segunda verificaçao se deseja depositar novamente
+                if (Opçao_Novo_Deposito == "sim" || Opçao_Novo_Deposito =="Sim" )
                 {
-                    double Nquantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    
+                    double Nquantia = double.Parse(Console.ReadLine());
+                    //leitura da quantia a ser depositada
                     conta.Deposito(Nquantia);
+                    //passando o atributo para o set do metodo
+                    conta = new Global(numero, nome, deposito);
+                    //instanciando o objeto novamente e quem sabe funcionar
                     Console.WriteLine("Dados atualizados: \n" + conta);
-
+                    // atualizando os dados do projeto
                     Console.WriteLine("Digite o quanto deseja sacar");
-                    if (conta.VerificaSaldo(Nquantia) == false)
-                    {
-                        string Opçao_Saque = Console.ReadLine();
-                        if (Opçao_Saque == "Sim" || Opçao_Saque == "sim")
+                    if (conta.VerificaSaldo(Nquantia) == false)//verifica se o saldo e negativo ou a quantia a ser sacada e menor do que se tem em caixa
+                    {                        
+                        string Opçao_Saque = Console.ReadLine();//leitura para verificar se ocorrera o sague se caso o mesmo passar 
+                        if (Opçao_Saque == "sim" || Opçao_Saque == "Sim")
                         {
+                            conta = new Global(numero, nome, deposito);
                             double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                            //leitura para saber quanto sera sacado
                             conta.Saque(saque);
+                            //passando o atributo saque por paramentro para o set do metodo
                             Console.WriteLine("dados atualizados: \n" + conta);
+                            //dados atualizados
                         }
                         else
                         {
                             conta = new Global(numero, nome);
+                            // opçao digitada e nao, entao nao foi realizada modificaçoes
                         }
+                    }
+                    else
+                    {
+                        Console.WriteLine(conta.VerificaSaldo(Nquantia));
+                        //mensagem do metodo
                     }
                 }
                 else
                 {
                     conta = new Global(numero, nome);
+                    //opçao digitada e nao, entao nao foi realizada modificaçoes
                 }
             }
             else
             {
                conta = new Global(numero, nome);
+                //opçao digitada e nao, entao nao foi realizada modificaçoes
             }
 
             Console.WriteLine();
