8ac80739b54427db9da64d365798d71582578ab9
samuel <sam226gold@gmail.com>
2019-02-01 16:30:45 -0400

    Adcionado um ultimo projeto a pasta de construtores

---
+++ c# poo/Construtores_Banco/Global.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Construtores_Banco
+{
+    class Global
+    {
+        private int _Conta;
+        public string _Titular { get; set; } 
+        public Double Saldo { get; private set; }
+
+        public Global()
+        {
+        }
+        public Global(int Conta, string Titular)
+        {
+            _Conta = Conta;
+            _Titular = Titular;           
+        }
+        public Global(int Conta, string Titular, double saldo) :this(Conta,Titular)
+        {
+            Saldo = saldo;
+        }
+        public void Deposito(Double quantia)
+        {
+            Saldo += quantia;
+        }
+        public bool VerificaSaldo(Double quantia)
+        {
+            if (quantia <= 0 || quantia<Saldo)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public void Saque(Double quantia)
+        {
+            Saldo -= quantia;
+        }
+        public override string ToString()
+        {
+           
+            return "Conta: "
+                + _Conta
+                + ", Titular: "
+                + _Titular
+                + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ c# poo/Construtores_Banco/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Construtores_Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Global conta;
+            Double quantia;
+
+            Console.WriteLine("Entre com o numero da conta");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o titular da conta");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Deseja depositar");
+            string Opçao = Console.ReadLine();
+
+            if(Opçao == "Sim" || Opçao =="sim")
+            {
+                Console.WriteLine("Digite o deposito inicial");
+                Double deposito = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                conta = new Global(numero, nome, deposito);
+
+                Console.WriteLine("voce deseja depositar novamente ?");
+                
+                string Opçao_New = Console.ReadLine();
+                if (Opçao_New == "Sim" || Opçao_New != "nao")
+                {
+                    double Nquantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    conta.Deposito(Nquantia);
+                    Console.WriteLine("Dados atualizados: \n" + conta);
+
+                    Console.WriteLine("Digite o quanto deseja sacar");
+                    if (conta.VerificaSaldo(Nquantia) == false)
+                    {
+                        string Opçao_Saque = Console.ReadLine();
+                        if (Opçao_Saque == "Sim" || Opçao_Saque == "sim")
+                        {
+                            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                            conta.Saque(saque);
+                            Console.WriteLine("dados atualizados: \n" + conta);
+                        }
+                        else
+                        {
+                            conta = new Global(numero, nome);
+                        }
+                    }
+                }
+                else
+                {
+                    conta = new Global(numero, nome);
+                }
+            }
+            else
+            {
+               conta = new Global(numero, nome);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados: \n"+conta);          
+
+            Console.ReadKey(true);
+        }
+    }
+}
