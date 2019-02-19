40625ddd16ccb9ba8660776e96cece9b7eed0192
samuel <sam226gold@gmail.com>
2019-02-19 17:24:58 -0400

    inhering

---
+++ c# poo_pt2/inhering/inhering/Entites/BusinessAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inhering.Entites
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+            
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double leonLimit) 
+            :base(number, holder, balance)
+        {
+            LoanLimit = leonLimit;
+        }
+
+        public void loan(double amounth)
+        {
+            if(amounth <= LoanLimit)
+            {
+                Balance += amounth;
+            }
+        }
+    }
+}
--- c# poo_pt2/inhering/inhering/Program.cs
+++ c# poo_pt2/inhering/inhering/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using inhering.Entites;
 namespace inhering
 {
     class Program
     {
         static void Main(string[] args)
         {
+            BusinessAccount account = new BusinessAccount(8010, "Bob", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+
+            Console.ReadKey(true);
         }
     }
 }
